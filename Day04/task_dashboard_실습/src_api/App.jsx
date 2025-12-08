ee10000ef4a213ea439fef7c77f78c55bb46ebaa
agnes-lec <whachozang@yonsei.ac.kr>
2025-12-08 16:07:28 +0900

    Delete Day04/task_dashboard_실습/src_api directory

--- Day04/task_dashboard_실습/src_api/App.jsx
+++
@@ -1,8 +0,0 @@
-import TaskApp from './components/TaskApp';
-import './App.css';
-
-function App() {
-  return <TaskApp />;
-}
-
-export default App;
--- Day04/task_dashboard_실습/src_api/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://13.220.93.143:8080',
-});
-
-export default api;
